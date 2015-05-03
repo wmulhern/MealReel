ec69ce3b5c69755370351c91bb6d7e0c5479d1a6
Whitney Mulhern <Whitney@172-17-32-254.DYNAPOOL.NYU.EDU>
2015-05-03 16:58:42 -0400

    Added more front end stuff. More usability

---
+++ MealReel/MatchViewController.h
@@ -0,0 +1,13 @@
+//
+//  MatchViewController.h
+//  MealReel
+//
+//  Created by Whitney on 5/3/15.
+//  Copyright (c) 2015 New York University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MatchViewController : UIViewController
+
+@end
---
+++ MealReel/SorryViewController.h
@@ -0,0 +1,13 @@
+//
+//  SorryViewController.h
+//  MealReel
+//
+//  Created by Whitney on 5/3/15.
+//  Copyright (c) 2015 New York University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SorryViewController : UIViewController
+
+@end
